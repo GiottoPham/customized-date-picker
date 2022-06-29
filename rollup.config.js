0c62694e0c23204ad1cac5dc6ee1ad823da590ce
Khang Nguyen <giotto2015.py@gmail.com>
2022-06-29 18:59:52 +0700

    fix for package

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,8 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
-
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import { terser } from "rollup-plugin-terser";
 const packageJson = require("./package.json");
 
 export default [
@@ -21,9 +22,11 @@ export default [
             },
         ],
         plugins: [
+            peerDepsExternal(),
             resolve(),
             commonjs(),
             typescript({ tsconfig: "./tsconfig.json" }),
+            terser(),
         ],
     },
     {
